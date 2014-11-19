a8e6d6770f109f65fc298160536419158edf3fcf
tim <llStrainll@gmail.com>
2014-11-19 13:39:45 +0300

    some refactoring

--- src/pmclj/core.clj
+++ src/pmclj/core.clj
@@ -6,26 +6,16 @@
              ~pattern true
              :else false))
 
-(defn pipe_matching_inner [{pattern :pattern, result :result, expressions :expressions}]
+(defn pipe_matching_inner [{pattern :pattern, result :result, expressions :expressions, continue_on_match :continue_on_match}]
       (case (or (= expressions nil) (= expressions ()))
             true result
             false  (let [to_pipe (first expressions) rest_expr (rest expressions)]
                         `(let [~'res ~result]
-                              (case (check_match ~'res ~pattern)
-                                    true ~(pipe_matching_inner {:pattern pattern, :result `(-> ~'res ~to_pipe), :expressions rest_expr})
+                              (case (= ~continue_on_match (check_match ~'res ~pattern))
+                                    true ~(pipe_matching_inner {:pattern pattern, :result `(-> ~'res ~to_pipe), :expressions rest_expr, :continue_on_match continue_on_match})
                                     false ~'res)))))
 
 (defmacro pipe_matching [pattern init_expression & other_expressions ]
-          (pipe_matching_inner {:pattern pattern, :result init_expression, :expressions other_expressions}))
-
-(defn pipe_not_matching_inner [{pattern :pattern, result :result, expressions :expressions}]
-      (case (or (= expressions nil) (= expressions ()))
-            true result
-            false  (let [to_pipe (first expressions) rest_expr (rest expressions)]
-                        `(let [~'res ~result]
-                              (case (check_match ~'res ~pattern)
-                                    false ~(pipe_not_matching_inner {:pattern pattern, :result `(-> ~'res ~to_pipe), :expressions rest_expr})
-                                    true ~'res)))))
-
+          (pipe_matching_inner {:pattern pattern, :result init_expression, :expressions other_expressions, :continue_on_match true}))
 (defmacro pipe_not_matching [pattern init_expression & other_expressions ]
-          (pipe_not_matching_inner {:pattern pattern, :result init_expression, :expressions other_expressions}))
+          (pipe_matching_inner {:pattern pattern, :result init_expression, :expressions other_expressions, :continue_on_match false}))
