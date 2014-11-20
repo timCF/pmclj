984412ec81f65d6cf142f30545709b873d0a777a
tim <llStrainll@gmail.com>
2014-11-20 15:30:37 +0300

    add pred_matching, key_matching, fix bug with result binding

--- src/pmclj/core.clj
+++ src/pmclj/core.clj
@@ -1,21 +1,67 @@
 (ns pmclj.core
     (:use [clojure.core.match :only (match)]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; predicate matching ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro pred_matching [pred init_expr & others_expr ]
+          (case (or (= others_expr nil) (= others_expr ()))
+                true   init_expr
+                false  `(let [res# ~init_expr]
+                             (case (~pred res#)
+                                   true  (pred_matching ~pred (-> res# ~(first others_expr)) ~@(rest others_expr))
+                                   false res#))))
+
+(defmacro pred_not_matching [pred init_expr & others_expr ]
+          (case (or (= others_expr nil) (= others_expr ()))
+                true   init_expr
+                false  `(let [res# ~init_expr]
+                             (case (~pred res#)
+                                   false  (pred_not_matching ~pred (-> res# ~(first others_expr)) ~@(rest others_expr))
+                                   true res#))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; full pattern matching ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defmacro check_match [obj pattern]
       `(match ~obj
              ~pattern true
              :else false))
 
-(defn pipe_matching_inner [{pattern :pattern, result :result, expressions :expressions, continue_on_match :continue_on_match}]
-      (case (or (= expressions nil) (= expressions ()))
-            true result
-            false  (let [to_pipe (first expressions) rest_expr (rest expressions)]
-                        `(let [~'res ~result]
-                              (case (= ~continue_on_match (check_match ~'res ~pattern))
-                                    true ~(pipe_matching_inner {:pattern pattern, :result `(-> ~'res ~to_pipe), :expressions rest_expr, :continue_on_match continue_on_match})
-                                    false ~'res)))))
+(defmacro pipe_matching [pattern init_expr & others_expr ]
+          (case (or (= others_expr nil) (= others_expr ()))
+                true   init_expr
+                false  `(let [res# ~init_expr]
+                             (case (check_match res# ~pattern)
+                                   true  (pipe_matching ~pattern (-> res# ~(first others_expr)) ~@(rest others_expr))
+                                   false res#))))
 
-(defmacro pipe_matching [pattern init_expression & other_expressions ]
-          (pipe_matching_inner {:pattern pattern, :result init_expression, :expressions other_expressions, :continue_on_match true}))
-(defmacro pipe_not_matching [pattern init_expression & other_expressions ]
-          (pipe_matching_inner {:pattern pattern, :result init_expression, :expressions other_expressions, :continue_on_match false}))
+(defmacro pipe_not_matching [pattern init_expr & others_expr ]
+          (case (or (= others_expr nil) (= others_expr ()))
+                true   init_expr
+                false  `(let [res# ~init_expr]
+                             (case (check_match res# ~pattern)
+                                   false (pipe_not_matching ~pattern (-> res# ~(first others_expr)) ~@(rest others_expr))
+                                   true  res#))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; simplified key matching ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro key_matching [key init_expr & others_expr ]
+          (case (or (= others_expr nil) (= others_expr ()))
+                true   init_expr
+                false  `(let [res# ~init_expr]
+                             (case (get res# ~key)
+                                   nil  res#
+                                   (key_matching ~key (-> res# ~(first others_expr)) ~@(rest others_expr))))))
+
+(defmacro key_not_matching [key init_expr & others_expr ]
+          (case (or (= others_expr nil) (= others_expr ()))
+                true   init_expr
+                false  `(let [res# ~init_expr]
+                             (case (get res# ~key)
+                                   nil  (key_not_matching ~key (-> res# ~(first others_expr)) ~@(rest others_expr))
+                                   res#))))
--- test/pmclj/core_test.clj
+++ test/pmclj/core_test.clj
@@ -2,37 +2,131 @@
     (:require [clojure.test :refer :all]
      [pmclj.core :refer :all]))
 
-(defn func1 [some_int]
-      (println "func1 called" some_int)
+
+
+(defn func1 [arg2]
+      (println "func1 called" arg2)
       {:ok "func1_returns"})
 
-(defn func2 [some_arg some_int]
-      (println "func2 called" some_arg some_int)
+(defn func2 [arg1 arg2]
+      (println "func2 called" arg1 arg2)
       {:fail "func2_returns"})
 
-(defn func3 [some_arg some_int]
-      (println "func3 called" some_arg some_int)
+(defn func3 [arg1 arg2]
+      (println "func3 called" arg1 arg2)
       {:ok "func3_returns"})
-
-(defn example1 []
-      (pipe_matching {:ok some} (func1 123) (func2 123) (func3 123)))
-(defn example2 []
-      (pipe_matching {:ok some} (func1 123) (func3 123) (func2 123)))
-(defn example3 []
-      (pipe_not_matching {:fail some} (func1 123) (func2 123) (func3 123)))
-(defn example4 []
-      (pipe_not_matching {:fail some} (func1 123) (func3 123) (func2 123)))
+(defn proxy_func [arg1 arg2] arg2)
 
 
-(deftest a-test1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; pred matching tests ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn example1_pred []
+      (pred_matching #(contains? % :ok) (func1 123) (func2 123) (func3 123)))
+(defn example2_pred []
+      (pred_matching #(contains? % :ok) (func1 123) (func3 123) (func2 123)))
+(defn example3_pred []
+      (pred_not_matching #(contains? % :fail) (func1 123) (func2 123) (func3 123)))
+(defn example4_pred []
+      (pred_not_matching #(contains? % :fail) (func1 123) (func3 123) (func2 123)))
+(defn example5_pred [] (let [res# "qwe"] (pred_matching #(contains? % :ok) (func1 res#) (func3 res#) (proxy_func res#))))
+(defn example6_pred [] (let [res "qwe"] (pred_matching #(contains? % :ok) (func1 res) (func3 res) (proxy_func res))))
+
+
+
+
+(deftest a-test1_pred
          (testing "example1"
-                  (is (= {:fail "func2_returns"} (example1)))))
-(deftest a-test2
+                  (is (= {:fail "func2_returns"} (example1_pred)))))
+(deftest a-test2_pred
          (testing "example2"
-                  (is (= {:fail "func2_returns"} (example2)))))
-(deftest a-test3
+                  (is (= {:fail "func2_returns"} (example2_pred)))))
+(deftest a-test3_pred
          (testing "example3"
-                  (is (= {:fail "func2_returns"} (example3)))))
-(deftest a-test4
+                  (is (= {:fail "func2_returns"} (example3_pred)))))
+(deftest a-test4_pred
          (testing "example4"
-                  (is (= {:fail "func2_returns"} (example4)))))
+                  (is (= {:fail "func2_returns"} (example4_pred)))))
+(deftest a-test5_pred
+         (testing "example5"
+                  (is (= "qwe" (example5_pred)))))
+(deftest a-test6_pred
+         (testing "example6"
+                  (is (= "qwe" (example6_pred)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; pipe matching tests ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn example1_pm []
+      (pipe_matching {:ok some} (func1 123) (func2 123) (func3 123)))
+(defn example2_pm []
+      (pipe_matching {:ok some} (func1 123) (func3 123) (func2 123)))
+(defn example3_pm []
+      (pipe_not_matching {:fail some} (func1 123) (func2 123) (func3 123)))
+(defn example4_pm []
+      (pipe_not_matching {:fail some} (func1 123) (func3 123) (func2 123)))
+(defn example5_pm [] (let [res# "qwe"] (pipe_matching {:ok some} (func1 res#) (func3 res#) (proxy_func res#))))
+(defn example6_pm [] (let [res "qwe"] (pipe_matching {:ok some} (func1 res) (func3 res) (proxy_func res))))
+
+
+
+
+(deftest a-test1_pm
+         (testing "example1"
+                  (is (= {:fail "func2_returns"} (example1_pm)))))
+(deftest a-test2_pm
+         (testing "example2"
+                  (is (= {:fail "func2_returns"} (example2_pm)))))
+(deftest a-test3_pm
+         (testing "example3"
+                  (is (= {:fail "func2_returns"} (example3_pm)))))
+(deftest a-test4_pm
+         (testing "example4"
+                  (is (= {:fail "func2_returns"} (example4_pm)))))
+(deftest a-test5_pm
+         (testing "example5"
+                  (is (= "qwe" (example5_pm)))))
+(deftest a-test6_pm
+         (testing "example6"
+                  (is (= "qwe" (example6_pm)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; key matching tests ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn example1_km []
+      (key_matching :ok (func1 123) (func2 123) (func3 123)))
+(defn example2_km []
+      (key_matching :ok (func1 123) (func3 123) (func2 123)))
+(defn example3_km []
+      (key_not_matching :fail (func1 123) (func2 123) (func3 123)))
+(defn example4_km []
+      (key_not_matching :fail (func1 123) (func3 123) (func2 123)))
+(defn example5_km [] (let [res# "qwe"] (key_matching :ok (func1 res#) (func3 res#) (proxy_func res#))))
+(defn example6_km [] (let [res "qwe"] (key_matching :ok (func1 res) (func3 res) (proxy_func res))))
+
+
+
+
+(deftest a-test1_km
+         (testing "example1"
+                  (is (= {:fail "func2_returns"} (example1_km)))))
+(deftest a-test2_km
+         (testing "example2"
+                  (is (= {:fail "func2_returns"} (example2_km)))))
+(deftest a-test3_km
+         (testing "example3"
+                  (is (= {:fail "func2_returns"} (example3_km)))))
+(deftest a-test4_km
+         (testing "example4"
+                  (is (= {:fail "func2_returns"} (example4_km)))))
+(deftest a-test5_km
+         (testing "example5"
+                  (is (= "qwe" (example5_km)))))
+(deftest a-test6_km
+         (testing "example6"
+                  (is (= "qwe" (example6_km)))))
